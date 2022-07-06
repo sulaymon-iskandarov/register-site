604ec2c60f857afa506196c6efdb221ff7837a21
sulaymon-iskandarov <108761090+sulaymon-iskandarov@users.noreply.github.com>
2022-07-06 17:11:58 +0500

    yangi fayllar qoshildi
    darslar yuklandi

---
+++ my_file/my_files.py
@@ -0,0 +1,23 @@
+#
+#
+# r (read)-o`qish
+# w (write)-yozish,bosh bolmasa ochirib ustiga yozadi
+# a (append)-faylga yozadi bosh bolmasa ,davomidan yozadi
+# x- fayl yaratadi.agar shu nomli fayl bolsa xatolik beradi
+
+
+# f=open("text_file3.txt", "x")
+# f.write("\n text ichiga yangi sozlar yozilgan!")
+# f.close()
+
+# f= open("text_file3.txt", "r")
+# print(f.read())
+#
+# import os
+# os.remove("text_file3.txt")
+
+a=33
+b=44
+if b>a:
+    pass #b soni katta a sonidan
+print("pass sozi bolmaganda bu soz print bolmas edi")
